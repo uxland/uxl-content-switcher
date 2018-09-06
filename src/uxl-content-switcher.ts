7503ffbd521e6e4bac832530f1a96d998491fc1a
Daniel Cabiscol <danielcabiscol@gmail.com>
2018-09-06 15:29:35 +0200

    new animations

--- src/uxl-content-switcher.ts
+++ src/uxl-content-switcher.ts
@@ -46,8 +46,10 @@ export class UxlContentSwitcher extends LitElement {
             let items = Array.from(this.items);
             items.forEach(i=>i.classList.remove('selected'));
             items[index].classList.add('selected');
-            this.fadeInAnimation(items[index], 2000);
+            // this.fadeInAnimation(items[index], 2000);
             // this.slideDownAnimation(items[index], 1000);
+            this.slideLeftAnimation(items[index], 1000);
+            // this.stretchLeftAnimation(items[index], 1500);
         }
     }
 
@@ -71,11 +73,38 @@ export class UxlContentSwitcher extends LitElement {
 
     slideDownAnimation(element, duration){
         let animation = element.animate([
-            {transform: 'translateY(-100%)'},
-            {transform: 'translateY(4%)'},
-            {transform: 'translateY(-4%)'},
-            {transform: 'translateY(0%)'}],
-            {duration: duration, fill: 'forwards', easing: 'ease-in'});
+            {transform: 'translateY(-100%)', offset: 0.0},
+            {transform: 'translateY(40%)', offset: 0.5},
+            {transform: 'translateY(7%)', offset: 0.65},
+            {transform: 'translateY(4%)', offset: 0.8},
+            {transform: 'translateY(2%)', offset: 0.95},
+            {transform: 'translateY(0%)', offset: 1.0}],
+            {duration: duration, fill: 'forwards', easing: 'ease'});
+        return animation;
+    }
+
+    slideLeftAnimation(element, duration){
+        let animation = element.animate([
+                {transform: 'translateX(150%)', offset: 0.0},
+                {transform: 'translateX(40%)', offset: 0.5},
+                {transform: 'translateX(7%)', offset: 0.65},
+                {transform: 'translateX(4%)', offset: 0.8},
+                {transform: 'translateX(2%)', offset: 0.95},
+                {transform: 'translateX(0%)', offset: 1.0}],
+            {duration: duration, fill: 'forwards', easing: 'ease-in-out'});
+        return animation;
+    }
+
+    stretchLeftAnimation(element, duration){
+        let animation = element.animate([
+                {transform: 'scaleX(0.3)', offset: 0.0},
+                {transform: 'scaleX(1.02)', offset: 0.4},
+                {transform: 'scaleX(0.98)', offset: 0.60},
+                {transform: 'scaleX(1.01)', offset: 0.80},
+                {transform: 'scaleX(0.98)', offset: 1},
+                {transform: 'scaleX(1.01)', offset: 0.8},
+                {transform: 'scaleX(1)', offset: 1}],
+            {duration: duration, fill: 'forwards', easing: 'ease-out'});
         return animation;
     }
 
