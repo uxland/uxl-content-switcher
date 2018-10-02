89c4ece612149652ebe4df7b0454eae4f8b5fd6e
Daniel Cabiscol <danielcabiscol@gmail.com>
2018-10-02 12:59:52 +0200

    fix: styles

--- src/uxl-content-switcher-styles.ts
+++ src/uxl-content-switcher-styles.ts
@@ -2,6 +2,10 @@ import {html} from '@polymer/lit-element/lit-element';
 import {TemplateResult} from 'lit-html';
 const innerStyle = html`
     <style>
+        :host{
+            display: flex;
+            flex: 1;
+        }
         :host > ::slotted(:not(slot):not(.selected)) {
             display: none !important;
         }         
